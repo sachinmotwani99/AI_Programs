244e1bb0bffeb82d7dc17df8bd7f14ca5dd7614f
sachinmotwani99 <63347647+sachinmotwani99@users.noreply.github.com>
2021-06-07 10:34:55 +0530

    Create Camal-Banana.py

---
+++ Camal-Banana.py
@@ -0,0 +1,15 @@
+total=int(input('Enter no. of bananas at starting: '))
+distance=int(input('Enter distance you want to cover: '))
+load_capacity=int(input('Enter max load capacity of your camel: '))
+lose=0
+start=total
+for i in range(distance):
+    while start>0:
+        start=start-load_capacity
+        if start==1:
+            lose=lose-1
+        lose=lose+2
+    lose=lose-1
+    start=total-lose
+print("The maximum number of bananas to be transported is: ")
+print(start)
